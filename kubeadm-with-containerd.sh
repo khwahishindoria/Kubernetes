4248da46ffcd2ea8914bc2c9b7d79dbd89c608f0
khwahishindoria <khwahishindoria@gmail.com>
2025-01-03 22:21:14 +0530

    Update kubeadm-with-containerd.sh

--- kubeadm-with-containerd.sh
+++ kubeadm-with-containerd.sh
@@ -10,8 +10,8 @@ sudo sysctl --system
 sysctl net.ipv4.ip_forward
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -21,7 +21,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update && sudo apt-get install containerd.io && systemctl enable --now containerd
+sudo apt-get update -y  && sudo apt-get install containerd.io -y && systemctl enable --now containerd
 
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
@@ -44,7 +44,6 @@ sysctl -p /etc/sysctl.conf
 
 
 cat <<EOF |  sudo tee /etc/containerd/config.toml
-
 disabled_plugins = []
 imports = []
 oom_score = 0
@@ -263,10 +262,10 @@ version = 2
   uid = 0
 EOF
 
-sudo systemctl restart containerd && systemctl status containerd
+sudo systemctl restart containerd
 
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 sudo mkdir -p -m 755 /etc/apt/keyrings
